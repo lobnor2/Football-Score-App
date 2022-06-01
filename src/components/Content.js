aaeb4741a476769fa7566ecfa4ef12bcb3e57c60
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2022-06-01 21:56:40 +0530

    added league

--- src/components/Content.js
+++ src/components/Content.js
@@ -9,14 +9,15 @@ const Content = () => {
       <div className='content-container'>
         <div className="tabs">
           <div className='tab-leagues' onClick={() =>setActive(true)}>
-              <h2>Leagues</h2>
+              <h2 style={{color:active ? '#c20114': null}}>Leagues</h2>
           </div>
           <div className='tab-standings' onClick={() =>setActive(false)}>
-              <h2>Standings</h2>
+              <h2 style={{color:!active ? '#c20114': null}}>Standings</h2>
           </div>
 
         </div>
-        { active ? <Leagues /> : <Standings /> }
+        {/* if active is true show leagues else show standings */}
+        { active ? <Leagues /> : <Standings /> } 
       </div>
     </>
 )
--- src/components/Leagues.js
+++ src/components/Leagues.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import '../App.css'
+import axios from 'axios';
+
 
 const Leagues = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://api-football-standings.azharimm.site/leagues').then(res => {
+            console.log(res.data.data);
+            setData(res.data.data)
+        })
+    }, [])
+
   return (
     <div className='leagues-container'>
-        leagues
+        {data.map((data) => (
+            <div key={data.id} className="league-div">
+                <img src={data.logos.light} alt="" />
+                <p>{data.name}</p>
+            </div>
+        ))}
     </div>
   )
 }
